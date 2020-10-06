dace3c62d490b493cb9b65b822ab2828d71067cb
smishra11 <msubhasish11@gmail.com>
2020-10-07 00:04:47 +0530

    working on Education page

--- src/components/Body/Education/Education.js
+++ src/components/Body/Education/Education.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import './Education.css';
+import Intermediate from '../../../assets/intermediate.jpg';
+import BachelorDegree from '../../../assets/bachelor_degree.jpg';
 
 class Education extends Component {
   render() {
@@ -14,15 +16,33 @@ class Education extends Component {
           </p>
         </div>
         <hr />
-        <div className="text-center">
-          <p>
-            <span className="education_1">Intermediate: </span>
-            Fakir mohan university, Balasore, Odisha
-          </p>
-          <p>
-            <span className="education_1">Bachelor Degree: </span> Council of
-            Higner secondary education, Odisha
-          </p>
+        <div className="row m-3">
+          <div className="col-md-6">
+            <div className="card">
+              <img className="card-img-top" src={Intermediate} alt="Card cap" />
+              <div className="card-body">
+                <p className="card-title education_title">Intermediate</p>
+                <p className="card-text text-center">
+                  Council of higher secondary education, Odisha
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card ml-4">
+              <img
+                className="card-img-top"
+                src={BachelorDegree}
+                alt="Card cap"
+              />
+              <div className="card-body">
+                <p className="card-title education_title">Bachelor Degree</p>
+                <p className="card-text text-center">
+                  Fakir mohan university, Balasore, Odisha
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
